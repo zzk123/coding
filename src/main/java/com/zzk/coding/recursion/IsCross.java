8674089e9bf6854097d0f3041d2fe8e766c21347
zzk123 <753809163@qq.com>
2020-10-28 00:39:07 +0800

    add

---
+++ src/main/java/com/zzk/coding/recursion/IsCross.java
@@ -0,0 +1,91 @@
+package com.zzk.coding.recursion;
+
+import com.google.gson.Gson;
+
+/**
+ * @program: coding
+ * @description:
+ * @author: zzk
+ * @create: 2020-10-27 23:21
+ */
+public class IsCross {
+
+    public static boolean isCross1(String str1, String str2, String aim){
+        if(str1 == null || str2 == null || aim == null){
+            return false;
+        }
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        char[] chaim = aim.toCharArray();
+        if(chaim.length != ch1.length + ch2.length){
+            return false;
+        }
+        boolean[][] dp = new boolean[ch1.length + 1][ch2.length + 1];
+        dp[0][0] = true;
+        for(int i = 1; i <= ch1.length; i++){
+            if(ch1[i-1] != chaim[i-1]){
+                break;
+            }
+            dp[i][0] = true;
+        }
+        System.out.println(new Gson().toJson(dp));
+        for(int j = 1; j <= ch2.length; j++){
+            if(ch2[j-1] != chaim[j-1]){
+                break;
+            }
+            dp[0][j] = true;
+        }
+        System.out.println(new Gson().toJson(dp));
+        for(int i = 1; i <= ch1.length; i++){
+            for(int j = 1; j <= ch2.length; j++){
+                if((ch1[i-1] == chaim[i+j-1] && dp[i-1][j])
+                    || (ch2[j-1] == chaim[i+j-1] && dp[i][j-1])){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        System.out.println(new Gson().toJson(dp));
+        return dp[ch1.length][ch2.length];
+    }
+
+    public static boolean isCross2(String str1, String str2, String aim){
+        if(str1 == null || str2 == null || aim == null){
+            return false;
+        }
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        char[] chaim = aim.toCharArray();
+        if(chaim.length != ch1.length + ch2.length){
+            return false;
+        }
+        char[] longs = ch1.length >= ch2.length ? ch1 : ch2;
+        char[] shorts = ch1.length < ch2.length ? ch1 : ch2;
+        boolean[] dp = new boolean[shorts.length + 1];
+        dp[0] = true;
+        for(int i = 1; i <= shorts.length; i++){
+            if(shorts[i-1] != chaim[i-1]){
+                break;
+            }
+            dp[i] = true;
+        }
+        for(int i = 1; i <= longs.length; i++){
+            dp[0] = dp[0] && longs[i-1] == chaim[i-1];
+            for(int j = 1; j <= shorts.length; j++){
+                if((longs[i-1] == chaim[i+j-1] && dp[j])
+                    || (shorts[j-1] == chaim[i+j-1]) && dp[j-1]){
+                    dp[j] = true;
+                }else{
+                    dp[j] = false;
+                }
+            }
+        }
+        return dp[shorts.length];
+    }
+
+    public static void main(String[] args) {
+        String str1 = "AB";
+        String str2 = "12";
+        String aim = "A12B";
+        isCross1(str1, str2, aim);
+    }
+}
---
+++ src/main/java/com/zzk/coding/recursion/MinCost.java
@@ -0,0 +1,97 @@
+package com.zzk.coding.recursion;
+
+/**
+ * @program: coding
+ * @description: 9.最小编辑代价
+ * @author: zzk
+ * @create: 2020-10-27 22:25
+ */
+public class MinCost {
+
+    /**
+     * 经典的动态规划方法
+     * 时间复杂度为O(M*N)，空间复杂度为O(M*N)
+     * @param str1
+     * @param str2
+     * @param ic
+     * @param dc
+     * @param rc
+     * @return
+     */
+    public int minCost1(String str1, String str2, int ic, int dc, int rc){
+        if(str1 == null || str2 == null){
+            return 0;
+        }
+        char[] chs1 = str1.toCharArray();
+        char[] chs2 = str2.toCharArray();
+        int row = chs1.length + 1;
+        int col = chs2.length + 1;
+        int[][] dp = new int[row][col];
+        for(int i = 1; i < row; i++){
+            dp[i][0] = dc * i;
+        }
+        for(int j = 1; j < col; j++){
+            dp[0][j] = ic * j;
+        }
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(chs1[i-1] == chs2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = dp[i-1][j-1] + rc;
+                }
+                dp[i][j] = Math.min(dp[i][j], dp[i][j-1] + ic);
+                dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + dc);
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    /**
+     * 结合空间压缩的动态规划法
+     * @param str1
+     * @param str2
+     * @param ic
+     * @param dc
+     * @param rc
+     * @return
+     */
+    public int minCost2(String str1, String str2, int ic, int dc, int rc){
+        if(str1 == null || str2 == null){
+            return 0;
+        }
+        char[] chs1 = str1.toCharArray();
+        char[] chs2 = str2.toCharArray();
+        char[] longs = chs1.length >= chs2.length ? chs1 : chs2;
+        char[] shorts = chs1.length < chs2.length ? chs1 : chs2;
+        //str2较长就交换ic和dc的值
+        if(chs1.length < chs2.length){
+            int tmp = ic;
+            ic = dc;
+            dc = tmp;
+        }
+        int[] dp = new int[shorts.length + 1];
+        for(int i = 1; i <= shorts.length; i++){
+            dp[i] = ic * i;
+        }
+        for(int i = 1; i <= longs.length; i++){
+            //pre表示左上角的值
+            int pre = dp[0];
+            dp[0] = dc  * i;
+            for(int j = 1; j <= shorts.length; j++){
+                //dp[j]没更新前先保持下来
+                int tmp = dp[j];
+                if(longs[i-1] == shorts[j-1]){
+                    dp[j] = pre;
+                }else{
+                    dp[j] = pre + rc;
+                }
+                dp[j] = Math.min(dp[j], dp[j-1]+ic);
+                dp[j] = Math.min(dp[j], tmp + dc);
+                //pre变成dp[j]没更新前的值
+                pre = tmp;
+            }
+        }
+        return dp[shorts.length];
+    }
+}
