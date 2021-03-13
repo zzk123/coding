6d691ea8b486a0e8e6ed9688c3dc22990ab6205b
zekaizhou <753809163@qq.com>
2021-03-14 00:24:46 +0800

    add

---
+++ src/main/java/com/zzk/coding/other/getNthFromChar.java
@@ -0,0 +1,14 @@
+package com.zzk.coding.other;
+
+/**
+ * @ClassName getNthFromChar
+ * @Description 1到n中1出现的次数
+ * @Author zzk
+ * @Date 2021/3/14 0:23
+ **/
+public class getNthFromChar {
+
+    public String getString(char[] chs, int n){
+
+    }
+}
