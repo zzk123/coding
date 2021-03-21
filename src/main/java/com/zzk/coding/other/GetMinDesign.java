a30f140f46495497df0dafc4acd1a70e770998f0
zekaizhou <753809163@qq.com>
2021-03-21 15:13:37 +0800

    add

---
+++ src/main/java/com/zzk/coding/other/GetMinDesign.java
@@ -0,0 +1,49 @@
+package com.zzk.coding.other;
+
+/**
+ * @ClassName GetMinDesign
+ * @Description 19.在有序旋转数组中找到最小值
+ * @Author zzk
+ * @Date 2021/3/17 23:14
+ **/
+public class GetMinDesign {
+
+    public static int getMin(int[] arr){
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = 0;
+        while(low < high){
+            if(low == high - 1){
+                break;
+            }
+            if(arr[low] < arr[high]){
+                return arr[low];
+            }
+            mid = (low + high)/2;
+            if(arr[low] > arr[mid]){
+                high = mid;
+                continue;
+            }
+            if(arr[mid] > arr[high]){
+                low = mid;
+                continue;
+            }
+            while(low < mid){
+                if(arr[low] == arr[mid]){
+                    low++;
+                }else if(arr[low] < arr[mid]){
+                    return arr[low];
+                }else{
+                    high = mid;
+                    break;
+                }
+            }
+        }
+        return Math.min(arr[low], arr[high]);
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {4,5,6,1,2,3};
+        getMin(ints);
+    }
+}
